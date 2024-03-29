27aad1c9d83f06f88f8aea5eefe73b95180b1d43
Abdallah Montassar_bellah <amontasar2@gmail.com>
2024-03-29 23:49:18 +0100

    architecture + ajouterAdmin

---
+++ src/main/java/com/terrain/reservationTerrain/service/AdminServiceImpl.java
@@ -0,0 +1,41 @@
+package com.terrain.reservationTerrain.service;
+
+import com.terrain.reservationTerrain.entity.Admin;
+import com.terrain.reservationTerrain.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+
+public class AdminServiceImpl implements AdminService{
+    @Autowired
+    AdminRepository adminRepository;
+
+    @Override
+    public Admin ajouterAdmin(Admin admin) {
+        return adminRepository.save(admin);
+    }
+
+    @Override
+    public Admin modifierAdmin(Admin admin) {
+        return adminRepository.save(admin);
+    }
+
+    @Override
+    public void supprimerAdmin(Long id) {
+        adminRepository.deleteById(id);
+
+    }
+
+    @Override
+    public List<Admin> afficherAdmin() {
+        return adminRepository.findAll();
+    }
+
+    @Override
+    public Optional<Admin> affchierAdminById(Long id) {
+        return adminRepository.findById(id);
+    }
+}
